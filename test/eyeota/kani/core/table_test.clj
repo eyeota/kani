fc97a01e2a76a6684479314643a7c71dff8ecc15
Ronald Suwandi <rsuwandi@eyeota.com>
2016-08-05 13:21:30 +0800

    Added license header in source code

--- test/eyeota/kani/core/table_test.clj
+++ test/eyeota/kani/core/table_test.clj
@@ -1,3 +1,19 @@
+;; Copyright (C) 2016 Eyeota
+
+;; This file is part of kani.
+
+;; kani is free software:  you can redistribute it and/or modify
+;; it under the terms of the GNU Lesser General Public License as published by
+;; the Free Software Foundation, version 3 of the License.
+
+;; kani is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU Lesser General Public License for more details.
+
+;; You should have received a copy of the GNU Lesser General Public License
+;; along with kani.  If not, see <http://www.gnu.org/licenses/>.
+
 (ns eyeota.kani.core.table-test
   (:require [clojure.test :refer :all]
             [eyeota.kani.core.common :refer :all]
